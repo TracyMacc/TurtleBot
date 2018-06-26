a9298b861bdb86dceba66ca220fdbcddfa364dfe
lishuangyi <39966182+lishuangyi@users.noreply.github.com>
2018-06-26 14:10:53 +0200

    Update Line_Follow.cpp

--- Line_Follow.cpp
+++ Line_Follow.cpp
@@ -1,2 +1,42 @@
-/* Name: Line Follow
-/* Description: Line follow Program, Image Msg from Topic "/camera/prossed"  
+/* Node Name: Line Follow
+/* Description: Line follow Program.  
+ * Subscribe Topic:  
+ *    /camera/prossed
+ * 
+ * Advertised Topics:
+ *    /camera/processed
+ *    /camera/processed_lowbw
+ *    /camera/overlay
+ *    /camera/overlay_lowbw
+ *    /linepoints
+ */
+
+
+//#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//#include <vector>
+//#include <list>
+
+#include "ros/ros.h"
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <dynamic_reconfigure/server.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+using namespace std;
+
+int bandwidthDivider = 0;
+
+image_transport::Publisher pub_proc;
+
+/* when Node subscrib Image from */
+void followCallback(const sensor_msgs::ImageConstPtr& msg){
+
+}
